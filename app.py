f370467d2a191a83ff1996e92c4c8f0b5d7a6528
Jacob Cannon <tug33296@temple.edu>
2021-03-02 23:13:33 -0500

    add logic to render home page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/")
 def home():
-	return "hello world"
+	return render_template('home.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
